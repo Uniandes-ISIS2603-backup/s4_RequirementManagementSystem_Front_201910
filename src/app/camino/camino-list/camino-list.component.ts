820f83f9ea2fc9af5af3b6a9b38195e6fb6ae9e3
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-03-31 20:37:39 -0500

    Modules + components created for drs and camino

---
+++ src/app/camino/camino-list/camino-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camino-list',
+  templateUrl: './camino-list.component.html',
+  styleUrls: ['./camino-list.component.css']
+})
+export class CaminoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
