bc297299ab857d1cc6c38ad932576c45363f9eb8
mpedroza96 <47084369+mpedroza96@users.noreply.github.com>
2019-04-08 12:42:21 -0500

    Update app-routing.module.ts

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '../stakeholder/stakeholder-edit/node_modules/@angular/common';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {NgxPermissionsGuard} from 'ngx-permissions';
 
