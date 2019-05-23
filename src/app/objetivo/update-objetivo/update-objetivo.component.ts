80d13ec085aede76fef74d206d4ad0521696b79c
Jorge A Esguerra A <ja.esguerra@uniandes.edu.co>
2019-05-23 10:24:16 -0500

    Fixed obj create

--- src/app/objetivo/update-objetivo/update-objetivo.component.ts
+++ src/app/objetivo/update-objetivo/update-objetivo.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Objetivo } from '../objetivo';
 import { ObjetivoService } from '../objetivo.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Route } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Requisito } from 'src/app/requisito/requisito';
 
 @Component({
   selector: 'app-update-objetivo',
@@ -10,39 +12,73 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class UpdateObjetivoComponent implements OnInit {
 
-  //Objetivo nuevo a ser creado
-  objetivo: Objetivo;
+  /**
+  * The component's constructor
+  * @param ProyectoService The project's service
+  * @param toastrService The toastr to show messages to the user 
+  */
+ constructor(private objetivoService: ObjetivoService,
+  private toastrService: ToastrService, private route: ActivatedRoute) { }
 
-  //Objeto que permite crear un objetivo a traves del formulario
-  objeto:any = {};
 
-  constructor(private servicio:ObjetivoService, private router : Router, private route: ActivatedRoute) { }
+/**
+* The output which tells the parent component
+* that the user no longer wants to create a project
+*/
+@Output() cancel = new EventEmitter();
 
-  ngOnInit() {
-  }
+/**
+* The output which tells the parent component
+* that the user updated a new project
+*/
+@Output() update = new EventEmitter();
 
-  //Se crea un objetivo del formulario y se actualiza con el id actual
-  crearObjetivo() {
-    const proyectoId = +this.route.snapshot.paramMap.get('proyectoId');
-    const objetivoId = +this.route.snapshot.paramMap.get('objetivoId');
-    this.objetivo = this.objeto;
-    this.servicio.updateObjetivo(proyectoId, objetivoId, this.objetivo).subscribe((obj)=> {
-      this.objetivo = obj;
-    })
-    // this.intermediateNavigation();
-    // this.finalDestination(id);
-  }
-  // // Métodos auxiliares para refrescar fetcheando desde DB. 
-  // intermediateNavigation()
-  // {
-  //   this.router.navigate(['/objetivos/', ]);
-  // }
-  // finalDestination(id: number)
-  // {
-  //   this.router.navigate(['/objetivos/', id]);
-  // }
-  // cancelarActualizacion(): void
-  // {
-  //   this.router.navigate(['/objetivos/']);
-  // }
+/**
+* The objetivo to edit
+*/
+objetivoDetail: Objetivo;
+
+/**
+* Retrieves the information of the project
+*/
+getObjetivo(id: number, oId: number): void {
+  this.objetivoService.getObjetivo(id, oId)
+      .subscribe(objetivo => {
+          this.objetivoDetail = objetivo;
+      });
+}
+
+/**
+* Updates the project's information
+*/
+editObjetivo(): void {
+  const idPr = +this.route.snapshot.paramMap.get('proyectoId');
+  this.objetivoService.updateObjetivo(idPr, this.objetivoDetail.id, this.objetivoDetail)
+      .subscribe(() => {
+          this.update.emit();
+          this.toastrService.success("The objetivo information was updated", "Objetivo edition");
+      });
+      console.log(this.objetivoDetail);
+}
+
+/**
+* Informs the parent component that the user no longer wants to update the project.
+*/
+cancelEdition(): void {
+  this.cancel.emit();
+}
+
+/**
+* The function which initializes the component
+*/
+ngOnInit() {
+  this.objetivoDetail= new Objetivo();
+}
+
+/**
+* The function which is called every time the user chooses to edit a different project
+*/
+ngOnChanges() {
+  this.ngOnInit();
+}
 }
