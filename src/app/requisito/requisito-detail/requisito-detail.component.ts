eb5dc7dc1e9f775a071b764d3f3bc9a4789b0469
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-05-25 03:02:40 -0500

    funciona hasta requisito, con casos de uso

--- src/app/requisito/requisito-detail/requisito-detail.component.ts
+++ src/app/requisito/requisito-detail/requisito-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Requisito } from '../requisito';
 import { RequisitoService } from '../requisito.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { Casodeuso } from '../../casodeuso/casodeuso';
+import { CasodeusoService } from '../../casodeuso/casodeuso.service';
+import { ModificarRequisitoComponent } from '../modificar-requisito/modificar-requisito.component';
 
  
 
@@ -13,14 +15,28 @@ import { Location } from '@angular/common';
 })
 export class RequisitoDetailComponent implements OnInit {
   
+  funcional : boolean; 
+  casosDeUso : Casodeuso[];
+  constructor(private router: Router, private reqService: RequisitoService, private route: ActivatedRoute, private casoDeUsoService : CasodeusoService) { }
 
-  req: Requisito;
-  constructor(private router: Router, private reqService: RequisitoService, private route: ActivatedRoute) { }
+
+  @ViewChild(ModificarRequisitoComponent) update: ModificarRequisitoComponent;
+
+  @Input() requisitoDetail : Requisito;
+
 
   //Al iniciar se obtiene el objetivo actual, que llega de la ruta activada (componente padre).
   ngOnInit() {
     this.getRequisito();
   }
+
+  /**
+   * Metodo que lehace update al requisito
+   */
+  upDateRequisito(){
+    this.update.actualizarRequisito();
+  }
+
 
   //Metodo que llama al servicio y pide el servicio actual con el id dado en la ruta
   getRequisito(): void 
@@ -29,7 +45,10 @@ export class RequisitoDetailComponent implements OnInit {
     const objetivoId = +this.route.snapshot.paramMap.get('objetivoId');
     const requisitoId = +this.route.snapshot.paramMap.get('requisitoId');
     this.reqService.getRequisito(proyectoId, objetivoId, requisitoId)
-      .subscribe(req => this.req = req);
+      .subscribe(req => this.requisitoDetail = req);
+    this.casoDeUsoService.getCasosdeuso(proyectoId, objetivoId, requisitoId).subscribe(casos => this.casosDeUso = casos);
+    this.esFuncional();
+
   }
 
   eliminar(): void
@@ -40,5 +59,15 @@ export class RequisitoDetailComponent implements OnInit {
     this.reqService.deleteRequisito(proyectoId, objetivoId, requisitoId).subscribe((res)=>{
       this.router.navigate(['/requisitos']);
     });
+  }
+
+  esFuncional(): boolean{
+    if (this.requisitoDetail.tipo === 'FUNCIONAL'){
+      this.funcional = true;
+    }
+    else{
+      this.funcional = false;
+    }
+    return this.funcional;
   }
 }
