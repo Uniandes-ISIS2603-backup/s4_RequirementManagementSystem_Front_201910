a7a9c6cba0e6e6a3320595ce08dfaf910a4ee5e8
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-05-25 04:19:37 -0500

    funciona hasta caso de uso :)

--- src/app/aprobacion/aprobacion.ts
+++ src/app/aprobacion/aprobacion.ts
@@ -8,18 +8,9 @@ export class Aprobacion {
      */
     id: number;
     /**
-     * The type of the approbation. 
-     * It can be "OBJETIVO" or "REQUISITO"
-     */
-    tipo: string;
-    /**
      * Name and Family name of the author of the approbation
      */
     autor: string;
-    /**
-     * The organization of the author who made the approbation.
-     */
-    organizacion: string; 
     /**
      * Date and time of creation of the approbation.
      */
@@ -29,13 +20,5 @@ export class Aprobacion {
      * It can  be: "Aprobado", "En revisión", "No aprobado".
      */
     estado: string;
-    /**
-     * Id of the requirement or objective.
-     */
-    id_aprobado: string;
-    /**
-     * Name of the requirement or objective.
-     */
-    nombre_aprobado: string;
     
 }
